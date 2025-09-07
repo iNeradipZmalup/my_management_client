a36f9a20b4c32af589b4042cc7efa16d30a0bfeb
Fikri-Rouzan <fikrirzn@gmail.com>
2025-09-07 13:58:13 +0700

    Add custom button widgets for primary, secondary, and delete actions

--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:my_management_client/presentation/widgets/custom_button.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,6 +14,18 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Login')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: ListView(
+        padding: EdgeInsets.all(30),
+        children: [
+          ButtonPrimary(onPressed: () {}, title: 'Login'),
+          const Gap(30),
+          ButtonSecondary(onPressed: () {}, title: 'Register'),
+          const Gap(30),
+          ButtonDelete(onPressed: () {}, title: 'Delete'),
+        ],
+      ),
+    );
   }
 }
