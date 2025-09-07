16c15b13000244cabfc397b41adcab5c43cd8a00
Fikri-Rouzan <fikrirzn@gmail.com>
2025-09-07 13:00:36 +0700

    Refactor main app structure to enhance theme configuration and improve readability

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_management_client/common/app_color.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
@@ -12,7 +14,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: AppColor.primary,
+        scaffoldBackgroundColor: AppColor.surface,
+        colorScheme: const ColorScheme.light(
+          primary: AppColor.primary,
+          secondary: AppColor.secondary,
+          surface: AppColor.surface,
+          surfaceContainer: AppColor.surfaceContainer,
+          error: AppColor.error,
+        ),
+        textTheme: GoogleFonts.interTextTheme(),
+        shadowColor: AppColor.primary.withValues(alpha: 0.3),
+      ),
       home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
