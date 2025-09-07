22f825068d4d5b4d9174b6985e92e79e5239fd27
Fikri-Rouzan <fikrirzn@gmail.com>
2025-09-07 17:37:05 +0700

    Add remote data sources for agenda, mood, solution, and user management

---
+++ lib/data/datasources/user_remote_data_source.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+import 'package:my_management_client/common/logging.dart';
+import 'package:my_management_client/core/api.dart';
+import 'package:http/http.dart' as http;
+import 'package:my_management_client/data/models/user_model.dart';
+
+class UserRemoteDataSource {
+  static Future<(bool, String)> register(
+    String name,
+    String email,
+    String password,
+  ) async {
+    Uri url = Uri.parse('${API.baseURL}/api/register.php');
+
+    try {
+      final response = await http.post(
+        url,
+        body: {'name': name, 'email': email, 'password': password},
+      );
+
+      fdLog.response(response);
+
+      final resBody = jsonDecode(response.body);
+      String message = resBody['message'];
+      bool success = response.statusCode == 201;
+
+      return (success, message);
+    } catch (e) {
+      fdLog.title('UserRemoteDataSource - register', e.toString());
+
+      return (false, 'Something went wrong');
+    }
+  }
+
+  static Future<(bool, String, UserModel?)> login(
+    String email,
+    String password,
+  ) async {
+    Uri url = Uri.parse('${API.baseURL}/api/login.php');
+
+    try {
+      final response = await http.post(
+        url,
+        body: {'email': email, 'password': password},
+      );
+
+      fdLog.response(response);
+
+      final resBody = jsonDecode(response.body);
+      String message = resBody['message'];
+
+      if (response.statusCode == 200) {
+        final data = resBody['data'];
+        final user = UserModel.fromJson(data['user']);
+        return (true, message, user);
+      }
+
+      return (false, message, null);
+    } catch (e) {
+      fdLog.title('UserRemoteDataSource - login', e.toString());
+
+      return (false, 'Something went wrong', null);
+    }
+  }
+}
