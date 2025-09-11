f9db6900e3c2fd454416b3540d4600849dcdf2e0
Fikri-Rouzan <fikrirzn@gmail.com>
2025-09-11 18:29:10 +0700

    Implement login functionality with input validation and session management

--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import 'package:my_management_client/common/app_color.dart';
+import 'package:my_management_client/common/enums.dart';
+import 'package:my_management_client/common/info.dart';
+import 'package:my_management_client/presentation/pages/dashboard_page.dart';
 import 'package:my_management_client/presentation/pages/register_page.dart';
 import 'package:my_management_client/presentation/widgets/custom_button.dart';
+import 'package:my_management_client/presentation/widgets/input_auth.dart';
+import 'package:my_management_client/presentation/widgets/top_clip_painter.dart';
+import '../controllers/login_controller.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -13,23 +21,189 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final loginController = Get.put(LoginController());
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void gotoRegister() {
+    Navigator.pushReplacementNamed(context, RegisterPage.routeName);
+  }
+
+  void login() async {
+    String email = emailController.text;
+    String password = passwordController.text;
+
+    if (email == '') {
+      Info.failed('Email must be filled');
+
+      return;
+    }
+
+    if (!GetUtils.isEmail(email)) {
+      Info.failed('Email not valid');
+
+      return;
+    }
+
+    if (password == '') {
+      Info.failed('Password must be filled');
+
+      return;
+    }
+
+    final state = await loginController.executeRequest(email, password);
+
+    if (state.statusRequest == StatusRequest.failed) {
+      Info.failed(state.message);
+
+      return;
+    }
+
+    if (state.statusRequest == StatusRequest.success) {
+      Info.success(state.message);
+
+      if (mounted) {
+        Navigator.pushReplacementNamed(context, DashboardPage.routeName);
+      }
+
+      return;
+    }
+  }
+
+  @override
+  void dispose() {
+    LoginController.delete();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
-      body: ListView(
-        padding: EdgeInsets.all(30),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [buildHeader(), const Gap(40), buildInput()],
+                  ),
+                  buildRegisterNavigation(),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildHeader() {
+    return Stack(
+      children: [
+        const Align(alignment: Alignment.topRight, child: TopClipPainter()),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gap(MediaQuery.paddingOf(context).top),
+            Image.asset(
+              'assets/images/logo_auth.png',
+              width: double.infinity,
+              fit: BoxFit.fitWidth,
+            ),
+            const Gap(45),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      color: AppColor.primary,
+                    ),
+                  ),
+                  Gap(10),
+                  Text(
+                    'Please enter your account to login',
+                    style: TextStyle(fontSize: 14, color: AppColor.textBody),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget buildInput() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
         children: [
-          ButtonPrimary(
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, RegisterPage.routeName);
-            },
-            title: 'Login',
+          InputAuth(
+            controller: emailController,
+            hint: 'Email',
+            icon: 'assets/icons/email.png',
           ),
-          const Gap(30),
-          ButtonSecondary(onPressed: () {}, title: 'Register'),
-          const Gap(30),
-          ButtonDelete(onPressed: () {}, title: 'Delete'),
+          const Gap(24),
+          InputAuth(
+            controller: passwordController,
+            hint: 'Password',
+            obscureText: true,
+            icon: 'assets/icons/lock.png',
+          ),
+          const Gap(32),
+          Obx(() {
+            final state = loginController.state;
+
+            if (state.statusRequest == StatusRequest.loading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            return SizedBox(
+              width: double.infinity,
+              child: ButtonPrimary(onPressed: login, title: 'Login'),
+            );
+          }),
+        ],
+      ),
+    );
+  }
+
+  Widget buildRegisterNavigation() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Don\'t have an account?',
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+              color: AppColor.textTitle,
+            ),
+          ),
+          const Gap(4),
+          InkWell(
+            onTap: gotoRegister,
+            child: const Text(
+              'Register Here',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+                color: AppColor.primary,
+                decoration: TextDecoration.underline,
+                decorationColor: AppColor.primary,
+              ),
+            ),
+          ),
         ],
       ),
     );
